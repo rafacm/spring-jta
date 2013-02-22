77a35efda50d5cece8e90de0c0019b936c936e47
Rafael Cordones Marcos <rafael.cordones@plexiti.com>
2013-02-22 13:11:40 +0100

    Reorder DDL to avoid DB creation errors.

--- src/main/resources/crm-mysql.sql
+++ src/main/resources/crm-mysql.sql
@@ -5,19 +5,6 @@ create table crm.customer (
   primary key (id)
 );
 
-create table crm.line_item (
-  id bigint not null auto_increment,
-  shipped date,
-  purchased date,
-  product_id bigint not null,
-  purchase_id bigint not null,
-  primary key (id),
-  foreign key line_item_ibfk_2 (purchase_id) references purchase(id),
-  foreign key line_item_ibfk_1 (product_id) references product(id)
-);
-CREATE INDEX product_id ON crm.line_item (product_id);
-CREATE INDEX purchase_id ON crm.line_item (purchase_id);
-
 create table crm.product (
   id bigint not null auto_increment,
   description varchar(255),
@@ -48,4 +35,17 @@ create table crm.purchase_fulfillment_log (
 );
 CREATE INDEX purchase_id ON crm.purchase_fulfillment_log (purchase_id);
 CREATE INDEX customer_id ON crm.purchase_fulfillment_log (customer_id);
+
+create table crm.line_item (
+  id bigint not null auto_increment,
+  shipped date,
+  purchased date,
+  product_id bigint not null,
+  purchase_id bigint not null,
+  primary key (id),
+  foreign key line_item_ibfk_2 (purchase_id) references purchase(id),
+  foreign key line_item_ibfk_1 (product_id) references product(id)
+);
+CREATE INDEX product_id ON crm.line_item (product_id);
+CREATE INDEX purchase_id ON crm.line_item (purchase_id);
 
